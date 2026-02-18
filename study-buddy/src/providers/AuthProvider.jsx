04624bacc1df53f5810d635c23a9f872d3eb3672
mishelld <mishelldub@gmail.com>
2026-02-18 10:57:01 +0200

    AuthProvider.jsx

---
+++ study-buddy/src/providers/AuthProvider.jsx
@@ -0,0 +1,87 @@
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../data/supabaseClient";
+
+export const AuthContext = createContext();
+
+function AuthProvider({ onAuthReady, children }) {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      setLoading(true);
+      try {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        setUser(session?.user ?? null);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        onAuthReady();
+        setLoading(false);
+      }
+    };
+
+    checkSession();
+  }, [navigate]);
+
+  const handleLogin = async (email, password) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        setError(error.message);
+        return;
+      }
+      setUser(data.user);
+      setUsername(email);
+      navigate("/Tweeter-2.0-Project/home");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleLogout = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        setError(error.message);
+        return;
+      }
+      navigate("/Tweeter-2.0-Project/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <AuthContext.Provider
+        value={{
+          user,
+          handleLogout,
+          handleLogin,
+          error,
+          loading,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+}
+
+export default AuthProvider;
