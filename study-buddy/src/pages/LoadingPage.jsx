59d533b47f7ac20259977c5e423c72b2a515da5c
mishelld <mishelldub@gmail.com>
2026-02-19 15:15:24 +0200

    Center loading indicator

--- study-buddy/src/pages/LoadingPage.jsx
+++ study-buddy/src/pages/LoadingPage.jsx
@@ -1,6 +1,16 @@
 import { Loader } from "@mantine/core";
 
 function LoadingPage() {
-  return <Loader color="blue" size="xl" />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Loader color="blue" size="xl" />{" "}
+    </div>
+  );
 }
 export default LoadingPage;
