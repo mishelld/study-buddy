06cedaf998b399ef7419889efb3cc4934c7e7abe
mishelld <mishelldub@gmail.com>
2026-02-19 16:24:24 +0200

    Update Navbar UI

--- study-buddy/src/components/Navbar.jsx
+++ study-buddy/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import { useContext } from "react";
 import { useState } from "react";
-import { Burger, Container, Group, Button } from "@mantine/core";
+import { Burger, Container, Group, Button, Flex } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./HeaderSimple.module.css";
 import "./Navbar.css";
@@ -35,21 +35,12 @@ function Navbar() {
   return (
     <header className={`${classes.header} navbar`}>
       <Container size="md" className={`${classes.inner}`}>
-        <IconBrandMessenger size={28} />
-        <Group gap={5} visibleFrom="xs">
-          {items}
+        <Flex justify="space-between" align="center" style={{ width: "100%" }}>
+          <Group>{items}</Group>
           <Button color="red" onClick={handleLogout}>
             Logout
           </Button>
-        </Group>
-
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="xs"
-          size="sm"
-          aria-label="Toggle navigation"
-        />
+        </Flex>
       </Container>
     </header>
   );
